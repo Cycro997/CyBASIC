ebef9911cd4fec98f97faf42f18b4035ac0a83b7
Cycro997 <152512005+Cycro997@users.noreply.github.com>
2024-04-16 23:47:17 +0200

    Version 0.0.1.

---
+++ shell.py
@@ -0,0 +1,21 @@
+from basic import run, VER_INFO
+from sys import stderr
+from time import sleep
+
+ver_num = VER_INFO["version"]
+rel_date = VER_INFO["release_date"]
+
+print("PyLang shell")
+print(f"{ver_num} ({rel_date.day}/{rel_date.month}/{rel_date.year})")
+print("Built using CodePulse's tutorial on YT")
+
+while 1:
+    text = input(">> ")
+
+    result, error = run("<stdin>", text)
+
+    if error is not None:
+        print(error.as_string(), file=stderr)
+        sleep(.5)
+    else:
+        print(result)
